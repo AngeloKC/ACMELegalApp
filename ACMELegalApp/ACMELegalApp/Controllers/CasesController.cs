2b8df70e6a6dabf3b342e23bf4dfb2bd18d7056d
Ben Angelo <ben.t.angelo@gmail.com>
2023-04-11 23:48:09 -0500

    initial commit

---
+++ ACMELegalApp/ACMELegalApp/Controllers/CasesController.cs
@@ -0,0 +1,98 @@
+﻿using ACMELegalApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace ACMELegalApp.Controllers
+{
+    public class CasesController : Controller
+    {
+        private readonly string _connectionString;
+
+        public CasesController(IConfiguration configuration)
+        {
+            _connectionString = "Server=LAPTOP-RVSGHA90\\SQLEXPRESS;Database=ACMELegalApp;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        }
+
+        public IActionResult AllCases()
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var cases = connection.Query<Case>("GetAllCases", commandType: CommandType.StoredProcedure).ToList();
+
+                return View(cases);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAllCases()
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var cases = connection.Query<Case>("GetAllCases", commandType: System.Data.CommandType.StoredProcedure).ToList();
+
+                return Ok(cases);
+            }
+        }
+
+        [HttpGet("{userID}")]
+        public IActionResult GetCasesByUser(int userID)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@UserID", userID);
+
+                var cases = connection.Query<Case>("GetCasesByUser", parameters, commandType: System.Data.CommandType.StoredProcedure).ToList();
+
+                return Ok(cases);
+            }
+        }
+
+        [HttpDelete("{caseID}")]
+        public IActionResult DeleteCase(int caseID)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@CaseID", caseID);
+
+                connection.Execute("DeleteCase", parameters, commandType: System.Data.CommandType.StoredProcedure);
+
+                return NoContent();
+            }
+        }
+
+        [HttpPut("{caseID}")]
+        public IActionResult UpdateCase(int caseID, [FromBody] Case updatedCase)
+        {
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var parameters = new DynamicParameters();
+                parameters.Add("@CaseID", caseID);
+                parameters.Add("@CaseNumber", updatedCase.CaseNumber);
+                parameters.Add("@CaseName", updatedCase.CaseName);
+                parameters.Add("@CaseDescription", updatedCase.CaseDescription);
+                parameters.Add("@FilingDate", updatedCase.FilingDate);
+                parameters.Add("@Status", updatedCase.Status);
+                parameters.Add("@AssignedTo", updatedCase.AssignedTo);
+
+                connection.Execute("UpdateCase", parameters, commandType: System.Data.CommandType.StoredProcedure);
+
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ ACMELegalApp/ACMELegalApp/Models/CaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ACMELegalApp.Models
+{
+    public class Case
+    {
+        public int CaseID { get; set; }
+        public string CaseNumber { get; set; }
+        public string CaseName { get; set; }
+        public string CaseDescription { get; set; }
+        public DateTime FilingDate { get; set; }
+        public string Status { get; set; }
+        public int AssignedTo { get; set; }
+    }
+}
