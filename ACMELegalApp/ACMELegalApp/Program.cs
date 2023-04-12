23a63d50acc44763aa4cb1ea9a316fb49277fe8a
Ben Angelo <ben.t.angelo@gmail.com>
2023-04-12 01:17:31 -0500

    Updating default route

--- ACMELegalApp/ACMELegalApp/Program.cs
+++ ACMELegalApp/ACMELegalApp/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Cases}/{action=AllCases}/{id?}");
 
 app.Run();
